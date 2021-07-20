412c32fb2d39fd45d31c316e75e8806bd4875215
Ardalan Samimi <ardalan.samimi@gmail.com>
2021-07-20 21:57:28 +0200

    Specify version of Network

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "Deta",
     products: [.library(name: "Deta", targets: ["Deta"])],
     dependencies: [
-        .package(url: "git@github.com:pkrll/Network.git", .branch("main"))
+        .package(url: "git@github.com:pkrll/Network.git", from: "1.0.0")
     ],
     targets: [
         .target(name: "Deta", dependencies: ["Network"]),
