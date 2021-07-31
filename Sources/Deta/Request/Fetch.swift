42dcb1f4218c7d14b3f3ed707e6a64d7d9b95279
Ardalan Samimi <ardalan.samimi@gmail.com>
2021-07-31 12:17:19 +0200

    Call all completion handlers on the main thread + documentation

--- Sources/Deta/Request/Fetch.swift
+++ Sources/Deta/Request/Fetch.swift
@@ -5,20 +5,23 @@
 import Foundation
 
 public struct Fetch {
-    /// The request model for the Fetch function.
+    /// The request for the `fetch(model:query:_)` method.
     public struct Request: Encodable {
-
-        public let query: [[String: AnyEncodable]]
-        public let limit: Int?
-        public let last: String?
-        
+        let query: [[String: AnyEncodable]]
+        let limit: Int?
+        let last: String?
+        /// The payload for the `update(key:payload:_)` method.
+        ///
+        /// - Parameter query:  A list of queries. See Deta documentation for more information on how to construct a query.
+        /// - Parameter limit:  Number of items to fetch. If set, minimum value must be 1.
+        /// - Parameter last:   The last key seen in a previous paginated response.
         public init(query: [[String: AnyEncodable]] = [], limit: Int? = nil, last: String? = nil) {
             self.query = query
             self.limit = limit
             self.last = last
         }
     }
-    
+    /// The response of the `fetch(model:query:_)` method.
     public struct Response<Item: Fetchable>: Decodable {
         public struct Paging: Decodable {
             /// Number of items returned
