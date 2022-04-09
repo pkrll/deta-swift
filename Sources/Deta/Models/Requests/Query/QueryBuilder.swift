2c616e6bc34154da85686d3928cc8d4cb58a2f91
Ardalan Samimi <ardalan.samimi@gmail.com>
2022-04-09 12:28:57 +0200

    Implement all resultBuilder methods

--- Sources/Deta/Models/Requests/Query/QueryBuilder.swift
+++ Sources/Deta/Models/Requests/Query/QueryBuilder.swift
@@ -1,7 +1,3 @@
-//
-// Deta
-//
-
 import Foundation
 
 @resultBuilder
@@ -9,19 +5,111 @@ public struct QueryBuilder {
     public static func buildBlock(_ components: Query) -> [Query] {
         [components]
     }
-    
+
     public static func buildBlock(_ components: Query...) -> [Query] {
         components
+    }
+    
+    public static func buildBlock(_ components: [Query]...) -> [Query] {
+        components.flatMap { $0 }
+    }
+
+    public static func buildBlock(_ components: [Query]) -> [Query] {
+        components
+    }
+    
+    public static func buildEither(first component: [Query]) -> [Query] {
+        component
+    }
+    
+    public static func buildEither(second component: [Query]) -> [Query] {
+        component
+    }
+
+    public static func buildExpression(_ expression: ()) -> [Query] {
+        []
+    }
+    
+    public static func buildExpression(_ expression: Query) -> [Query] {
+        [expression]
+    }
+    
+    public static func buildOptional(_ component: [Query]?) -> [Query] {
+        component ?? []
+    }
+    
+    public static func buildBlock(_ components: [Query?]) -> [Query] {
+        components.compactMap { $0 }
+    }
+    
+    public static func buildExpression(_ expression: Query?) -> [Query] {
+        guard let expression = expression else {
+            return []
+        }
+        
+        return [expression]
+    }
+    
+    public static func buildExpression(_ expression: [Query]?) -> [Query] {
+        expression ?? []
     }
 }
 
 @resultBuilder
-public struct GroupingQueryBuilder {    
-    public static func buildBlock(_ components: Query...) -> [GroupedQueries] {
-        [GroupedQueries(components)]
+public struct GroupingQueryBuilder {
+    public static func buildBlock(_ components: GroupedQueries) -> [GroupedQueries] {
+        [components]
     }
-    
+
     public static func buildBlock(_ components: GroupedQueries...) -> [GroupedQueries] {
         components
+    }
+    
+    public static func buildBlock(_ components: [GroupedQueries]...) -> [GroupedQueries] {
+        components.flatMap { $0 }
+    }
+
+    public static func buildBlock(_ components: [GroupedQueries]) -> [GroupedQueries] {
+        components
+    }
+    
+    public static func buildEither(first component: [GroupedQueries]) -> [GroupedQueries] {
+        component
+    }
+    
+    public static func buildEither(second component: [GroupedQueries]) -> [GroupedQueries] {
+        component
+    }
+
+    public static func buildExpression(_ expression: ()) -> [GroupedQueries] {
+        []
+    }
+    
+    public static func buildExpression(_ expression: GroupedQueries) -> [GroupedQueries] {
+        [expression]
+    }
+    
+    public static func buildOptional(_ component: [GroupedQueries]?) -> [GroupedQueries] {
+        component ?? []
+    }
+    
+    public static func buildBlock(_ components: [GroupedQueries?]) -> [GroupedQueries] {
+        components.compactMap { $0 }
+    }
+    
+    public static func buildExpression(_ expression: GroupedQueries?) -> [GroupedQueries] {
+        guard let expression = expression else {
+            return []
+        }
+        
+        return [expression]
+    }
+    
+    public static func buildExpression(_ expression: [GroupedQueries]?) -> [GroupedQueries] {
+        expression ?? []
+    }
+    
+    public static func buildBlock(_ components: Query) -> [GroupedQueries] {
+        [GroupedQueries([components])]
     }
 }
